e7245ea66365ac2ad69d6d576d4a0ee65a0efdf0
A K Verma <amitkkc01@gmail.com>
2025-05-28 21:58:49 +0530

    Update provider.tf

--- sa_container_create/provider.tf
+++ sa_container_create/provider.tf
@@ -10,9 +10,9 @@ terraform {
 provider "azurerm" {
   # Configuration options
   features {}
-  subscription_id = "b7df65c6-7c4d-4e35-983d-cd66eea0573a"
-  client_id       = "a55665df-4001-4e2e-8975-860cbc1eaf0f"
-  client_secret   = "g-m8Q~lB6O21A4lLBmv9.31KbvaCyK~uep.TOafW"
-  tenant_id       = "628e55e0-ddbc-4fb1-84df-bb0350248525"
+  subscription_id = "b7df65cvc6-7vncc4d-4e35bvn-98cvn3d-cd66eea0573a"
+  client_id       = "a55665df-400cv1-4e2bveb-cn8975-860cbc1eaf0f"
+  client_secret   = "g-m8Q~lB6O21A4lLBmbnv9.31KbvaCyK~uep.TOafW"
+  tenant_id       = "628e55e0vnbcn-ddbc-4fb1-84df-bb0350248525"
 }
 
